d1beea035c86f00882922fa24966bca332bb420d
Jan Fic <jan.fic18@ncf.edu>
2022-06-15 17:30:04 -0400

    init commit

---
+++ desktop/src/com/janfic/games/library/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.janfic.games.library;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.janfic.games.library.JanFixelDriver;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("JanFixelsLibrary");
+		new Lwjgl3Application(new JanFixelDriver(), config);
+	}
+}
