56ddcd92c1b8f47d5e5f51a015aae483b1bf4685
Jan Fic <jan.fic18@ncf.edu>
2022-06-21 17:29:47 -0400

    removed redundant components

--- core/src/com/janfic/games/library/ecs/components/ui/InputListenerComponent.java
+++
@@ -1,9 +0,0 @@
-package com.janfic.games.library.ecs.components.ui;
-
-import com.badlogic.ashley.core.Component;
-import com.badlogic.ashley.core.Entity;
-import com.badlogic.gdx.scenes.scene2d.InputListener;
-
-public class InputListenerComponent implements Component {
-    public InputListener inputListener;
-}
