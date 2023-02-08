c70e10d296b638e65cb6d18876ee24d465e0563b
Jan Fic <jan.fic18@ncf.edu>
2023-02-08 08:19:56 -0500

    changed world ramp / height

--- dddserver/src/com/janfic/games/dddserver/worldsim/HexWorld.java
+++ dddserver/src/com/janfic/games/dddserver/worldsim/HexWorld.java
@@ -1,25 +1,19 @@
 package com.janfic.games.dddserver.worldsim;
 
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.math.MathUtils;
 import com.janfic.games.library.utils.ColorRamp;
-import de.articdive.jnoise.generators.noise_parameters.fade_functions.FadeFunction;
 import de.articdive.jnoise.generators.noise_parameters.interpolation.Interpolation;
 import de.articdive.jnoise.generators.noisegen.perlin.PerlinNoiseGenerator;
-import de.articdive.jnoise.pipeline.JNoise;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.function.Function;
 
 public class HexWorld {
 
     public Polyhedron polyhedron;
-
+    public float height;
     float posX, posY;
     int level;
-    public float height;
     ColorRamp ramp;
 
     public HexWorld(float height, float sx, float sy, int level) {
@@ -32,24 +26,25 @@ public class HexWorld {
         for (int i = 0; i < 9; i++) {
             polyhedron = Polyhedron.dual(polyhedron);
             polyhedron = Polyhedron.uniformTruncate(polyhedron);
-            if(i==3) {
+            if (i == 3) {
                 polyhedron = Polyhedron.sphereProject(polyhedron, height / 2);
             }
         }
         ramp = new ColorRamp();
-        ramp.addColor(0 / 20f, Color.BLUE);
-        ramp.addColor(5 / 20f, Color.BLUE);
-        ramp.addColor(9 / 20f, Color.SKY);
-        ramp.addColor(10 / 20f, Color.YELLOW);
-        ramp.addColor(11 / 20f, Color.FOREST);
-        ramp.addColor(14 / 20f, Color.OLIVE);
-        ramp.addColor(17 / 20f, Color.GRAY);
-        ramp.addColor(19 / 20f, Color.WHITE);
-        generateTerrain(1/ 4f, f -> f * 2, 4, 1);
-        normalizeTerrain(polyhedron.getMinHeight(), polyhedron.getMaxHeight(), 0, 1, 20);
-        colorTerrain();
+        float max = 4;
+        float steps = 10;
+        ramp.addColor(0 * max, Color.BLUE);
+        ramp.addColor(0.25f * max, Color.BLUE);
+        ramp.addColor(0.45f * max, Color.SKY);
+        ramp.addColor(0.50f * max, Color.YELLOW);
+        ramp.addColor(0.55f * max, Color.FOREST);
+        ramp.addColor(0.70f * max, Color.OLIVE);
+        ramp.addColor(0.85f * max, Color.SLATE);
+        ramp.addColor(0.95f * max, Color.WHITE);
+        generateTerrain(1 / 6f, f -> f * 2, 4, 1);
+        normalizeTerrain(polyhedron.getMinHeight(), polyhedron.getMaxHeight(), 0, max, (int) steps);
+        colorTerrain(0.45f * max);
     }
-
 
 
     private void normalizeTerrain(float minHeight, float maxHeight, float newMin, float newMax, int steps) {
@@ -58,20 +53,19 @@ public class HexWorld {
         for (Face face : polyhedron.faces) {
             face.height = transform.apply(face.height);
             float r = face.height % stepSize;
-            if(r - stepSize / 2 > 0) {
+            if (r - stepSize / 2 > 0) {
                 face.height += stepSize - r;
-            }
-            else {
+            } else {
                 face.height -= r;
             }
         }
     }
 
-    private void colorTerrain() {
+    private void colorTerrain(float waterLevel) {
         for (Face face : polyhedron.faces) {
             face.setColor(ramp.getColor(face.height));
-            if(face.height <= 0.45f) {
-                face.setHeight(0.45f);
+            if (face.height <= waterLevel) {
+                face.setHeight(waterLevel);
             }
         }
     }
@@ -93,7 +87,6 @@ public class HexWorld {
     }
 
 
-
     public void generateTerrain(float baseScale, Function<Float, Float> octaveFunction, int octaves, float amplitude) {
         PerlinNoiseGenerator generator = PerlinNoiseGenerator.newBuilder().setSeed(MathUtils.random(Integer.MAX_VALUE - 1)).setInterpolation(Interpolation.COSINE).build();
         float noiseScale = baseScale;
@@ -106,14 +99,14 @@ public class HexWorld {
             for (int i = 0; i < octaves; i++) {
                 float f = (float) generator.evaluateNoise(face.center.x * noiseScale, face.center.y * noiseScale, face.center.z * noiseScale);
                 f = (f + 1) / 2f;
-                f /= (i+1);
+                f /= (i + 1);
                 height += f;
                 noiseScale = octaveFunction.apply(noiseScale);
             }
 
             face.setHeight(height);
-            if(minHeight > height) minHeight = height;
-            if(maxHeight < height) maxHeight = height;
+            if (minHeight > height) minHeight = height;
+            if (maxHeight < height) maxHeight = height;
             average += height;
         }
         average /= polyhedron.faces.size();
